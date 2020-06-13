e25a2f3146c2bfa6648009be65f50f057ebe4e02
Seth Near <seth.j.near@gmail.com>
2020-06-13 22:44:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,14 +3,19 @@ def badge_maker(name)
 end
 
 def assign_rooms(speakers)
-
+  rooms = []
+  speakers.each_with_index { |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    }
+  return rooms
 end
 
 
 def batch_badge_creator(speakers)
-
+  return speakers.collect { |name| badge_maker(name)}
 end
 
-def printer(badges, rooms)
-
+def printer(speakers)
+  batch_badge_creator(speakers).inspect
+  assign_rooms(speakers).inspect
 end
